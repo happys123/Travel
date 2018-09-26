3aa06497b8c20effdaf25bfae79fd3718523839c
happys123 <chengeng176@163.com>
2018-09-26 18:51:31 +0800

    add header

---
+++ travel/src/pages/home/Home.vue
@@ -0,0 +1,19 @@
+<template>  <!-- 模板下面对外暴露的只能有一个元素，所以加div标签加包裹 -->
+    <div>
+        <home-header></home-header>
+    </div>
+</template>
+
+<script>
+import HomeHeader from './components/Header'
+export default {
+  name: 'Home',
+  components: {
+    HomeHeader: HomeHeader
+  }
+}
+</script>
+
+<style>
+
+</style>
