9f8937e951874c990ebebfa3569276a3626c2798
happys123 <chengeng176@163.com>
2018-09-26 21:42:39 +0800

    add swiper

--- travel/src/pages/home/Home.vue
+++ travel/src/pages/home/Home.vue
@@ -1,15 +1,18 @@
 <template>  <!-- 模板下面对外暴露的只能有一个元素，所以加div标签加包裹 -->
     <div>
         <home-header></home-header>
+        <home-swiper></home-swiper>
     </div>
 </template>
 
 <script>
 import HomeHeader from './components/Header'
+import HomeSwiper from './components/Swiper'
 export default {
   name: 'Home',
   components: {
-    HomeHeader: HomeHeader
+    HomeHeader: HomeHeader,
+    HomeSwiper
   }
 }
 </script>
---
+++ travel/src/pages/home/components/Swiper.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="wrapper">
+        <swiper :options="swiperOption">
+            <swiper-slide v-for="item of swiperList" :key="item.id">
+                <img class="swiper-img" :src="item.imgUrl" />
+            </swiper-slide>
+            <div class="swiper-pagination"  slot="pagination"></div>
+        </swiper>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data: function() {
+      return {
+          swiperOption: {
+              pagination: '.swiper-pagination',
+              loop: true
+          },
+          swiperList: [{
+              id: '0001',
+              imgUrl: 'https://img1.qunarzz.com/vc/06/34/b1/de6b421a416effd38e1c94973e.jpg'
+          },{
+              id: '0002',
+              imgUrl: 'https://img1.qunarzz.com/vc/f3/ea/ae/095dd61c09f2797ba9147bd03f.jpg'
+          }]
+      }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+/*>>>使样式能够不受scoped命令影响，在swiper组件内也起作用*/
+.wrapper >>> .swiper-pagination-bullet-active
+    background: #fff
+.wrapper
+    width: 100%
+    height: 0
+    overflow: hidden
+    padding-bottom: 30.47%
+    background: #eee
+    .swiper-img
+        width: 100%
+</style>
